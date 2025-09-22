12a493ec2534dc1a33e07768477785408e9a2d23
Enrique Diaz Blanco <qdiazblanco@gmail.com>
2025-09-22 12:36:24 +0000

    optimized init

--- sudoku_solver/Sudoku.py
+++ sudoku_solver/Sudoku.py
@@ -6,18 +6,28 @@ class Sudoku :
     
     data: list[list[int]]
 
+    def is_sudoku_digit(i : int) -> bool:
+        return i in SUDOKU_DIGITS
+
+    def is_blank(i : int) -> bool:
+        return i == BLANK_SYMBOL
+
+    def is_valid_digit(i : int) -> bool:
+        return is_sudoku_digit(i) or is_blank(i)
+    
+     
+
     def __init__(self, given_data : list[list[int]]) -> void:
 
         if not isinstance(given_data, list[list[int]]) :
             raise ValueError("Sudoku must be a matrix")
 
-        if not len(given_data) == 9 & not all(len(row) == 9 for row in given_data) :
-            raise ValueError("Sudoku must be a 9x9 matrix")
+        if not len(given_data) == SIZE & not all(len(row) == SIZE for row in given_data) :
+            raise ValueError(f"Sudoku must be a {SIZE}x{SIZE} matrix")
+
+        if not all(is_valid_digit(digit) for row in given_data for digit in row) :
+            raise ValueError(f"All Sudoku digits must be in {SUDOKU_DIGITS} or be a {BLANK_SYMBOL}")
         
-        for j in range(9) :
-            for i in range(9) :
-                if not given_data[j][i] < 10 & given_data[j][i] >= 0:
-                    raise ValueError("All digits must be between 0 and 9") 
-
-
-
+        #comprobar si es valido (dos iguales en misma fila por ej)
+        
+        self.data = given_data
